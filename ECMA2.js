5fb78c19f5bbe832dc98e011eae3c886b01953ec
Rumaisa_10 <sultanarumaisa12@gmail.com>
2021-12-04 21:10:19 +0600

    ECMA SCRIPT

---
+++ ECMA2.js
@@ -0,0 +1,11 @@
+let poorcountry = ["bangladesh", "sri-lanka"];
+let richcountry = [...poorcountry, "america,canada"];
+
+console.log(richcountry);
+
+let poorcountry1 = ["india", "bhutan"];
+let richcountry1 = ["spain", "italy"];
+
+richcountry1.push(poorcountry1);
+
+console.log(richcountry1);
